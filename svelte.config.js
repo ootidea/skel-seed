f67cad1d2e69b6c39e8a1d75db0880514e502466
ootidea <ootidea@gmail.com>
2022-04-12 18:24:35 +0900

    setup CSS function oklch

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import postcssOKLabFunction from '@csstools/postcss-oklab-function'
+import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: preprocess({
+    postcss: {
+      plugins: [postcssOKLabFunction({ subFeatures: { displayP3: false } })],
+    },
+  }),
 
   kit: {
-    adapter: adapter()
-  }
-};
+    adapter: adapter(),
+  },
+}
 
-export default config;
+export default config
