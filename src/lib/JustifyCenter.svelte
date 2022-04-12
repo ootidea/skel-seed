ec420b4527f13669683176a09c0f6dc6a6bfa355
ootidea <ootidea@gmail.com>
2022-04-12 20:38:29 +0900

    add JustifyCenter and JustifyEnd

---
+++ src/lib/JustifyCenter.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let klass = ''
+  export { klass as class }
+</script>
+
+<div class="skeleto-justify-center_root {klass}">
+  <slot />
+</div>
+
+<style global lang="scss">
+  .skeleto-justify-center_root {
+    display: flex;
+    justify-content: center;
+  }
+</style>
---
+++ src/lib/JustifyEnd.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let klass = ''
+  export { klass as class }
+</script>
+
+<div class="skeleto-justify-end_root {klass}">
+  <slot />
+</div>
+
+<style global lang="scss">
+  .skeleto-justify-end_root {
+    display: flex;
+    justify-content: end;
+  }
+</style>
