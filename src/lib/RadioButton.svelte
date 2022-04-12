fec3deb8a88b0a912ea938861977cfcc3d3ecb34
ootidea <ootidea@gmail.com>
2022-04-12 23:05:40 +0900

    add RadioButton

---
+++ src/lib/RadioButton.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import CommonCss from './CommonCss.svelte'
+
+  export let group = ''
+  export let value: string | undefined = undefined
+  export let disabled = false
+
+  let klass = ''
+  export { klass as class }
+</script>
+
+<label class="skeleto-radio-button_root {klass}" class:skeleto-disabled={disabled}>
+  <input type="radio" class="skeleto-radio-button_radio" bind:group {value} {disabled} />
+  <slot />
+</label>
+
+<CommonCss />
+
+<style global lang="scss">
+  .skeleto-radio-button_root {
+    display: inline-grid;
+    grid-auto-flow: column;
+    align-items: center;
+    gap: 0.3em;
+
+    cursor: pointer;
+
+    &.skeleto-disabled {
+      cursor: default;
+
+      color: oklch(70% 0 0);
+    }
+  }
+
+  .skeleto-radio-button_radio {
+    margin: 0;
+    accent-color: var(--accent-color);
+
+    cursor: pointer;
+
+    .skeleto-disabled & {
+      cursor: default;
+    }
+  }
+</style>
