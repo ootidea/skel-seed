b16e618a7260ecc10281b78b1fd48388a249939e
ootidea <ootidea@gmail.com>
2022-04-12 11:33:07 +0900

    add HeaderLayout and FooterLayout

---
+++ src/lib/FooterLayout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let orientation: 'horizontal' | 'vertical' = 'horizontal'
+  let klass = ''
+  export { klass as class }
+</script>
+
+<div class="root {orientation} {klass}">
+  <slot />
+</div>
+
+<style lang="scss">
+  .root {
+    display: grid;
+
+    &.horizontal {
+      width: 100%;
+      grid-template-columns: minmax(0, 1fr) auto;
+    }
+
+    &.vertical {
+      height: 100%;
+      grid-template-rows: minmax(0, 1fr) auto;
+    }
+  }
+</style>
---
+++ src/lib/HeaderLayout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let orientation: 'horizontal' | 'vertical' = 'horizontal'
+  let klass = ''
+  export { klass as class }
+</script>
+
+<div class="root {orientation} {klass}">
+  <slot />
+</div>
+
+<style lang="scss">
+  .root {
+    display: grid;
+
+    &.horizontal {
+      width: 100%;
+      grid-template-columns: auto minmax(0, 1fr);
+    }
+
+    &.vertical {
+      height: 100%;
+      grid-template-rows: auto minmax(0, 1fr);
+    }
+  }
+</style>
