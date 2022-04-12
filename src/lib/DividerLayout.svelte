cd2d468a50f1c097168ba861597936194652220b
ootidea <ootidea@gmail.com>
2022-04-12 11:50:18 +0900

    add DividerLayout

---
+++ src/lib/DividerLayout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  type Item = $$Generic<unknown>
+  export let items: readonly Item[]
+
+  let klass = ''
+  export { klass as class }
+</script>
+
+<div class="divider-layout_root {klass}">
+  {#each items as item, index}
+    {#if index > 0}
+      <slot name="divider">
+        <hr />
+      </slot>
+    {/if}
+    <slot name="item" {item} {index} />
+  {/each}
+</div>
