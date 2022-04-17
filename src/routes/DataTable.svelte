1a201cce505d0573a48c0075a122fb7417c2473a
ootidea <ootidea@gmail.com>
2022-04-17 11:57:44 +0900

    add DataTable page

---
+++ src/routes/DataTable.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import DataTable from '../lib/DataTable.svelte'
+  import DataTableCell from '../lib/DataTableCell.svelte'
+
+  const rows = [
+    { name: 'hoge', age: 10, createdAt: new Date(), ed: true, image: 'src/assets/plus.svg' },
+    { name: 'eccc', age: 1219, createdAt: new Date(), ed: false, image: 'src/assets/check.svg' },
+    { name: 'pien', age: 32, createdAt: new Date(), ed: false, image: 'src/assets/clipboard.svg' },
+    { name: 'fontor', age: 10, createdAt: new Date(), ed: true, image: 'https://example.com' },
+    { name: 'bruky', age: 19, createdAt: new Date(), ed: false, image: 'src/assets/gear.svg' },
+    { name: 'ape', age: 32, createdAt: new Date(), ed: true, image: 'src/assets/page.svg' },
+  ]
+</script>
+
+<div>
+  <DataTable
+    {rows}
+    columns={['行番号', 'name', { id: 'age', title: '年齢' }, 'ed', 'image', 'createdAt']}
+  >
+    <svelte:fragment slot="cell" let:columnId let:row let:value let:rowIndex>
+      {#if columnId === '行番号'}
+        <DataTableCell value={rowIndex} />
+      {:else}
+        <DataTableCell {value} />
+      {/if}
+    </svelte:fragment>
+  </DataTable>
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import Divider from '../lib/Divider.svelte'
+  import Link from '../lib/Link.svelte'
+  import StretchLayout from '../lib/StretchLayout.svelte'
+</script>
+
+<StretchLayout class="full-height" stretchAt={2}>
+  <div class="sidebar">
+    <Link href="./">index</Link>
+    <Link href="DataTable" />
+  </div>
+  <Divider direction="vertical" />
+  <slot />
+</StretchLayout>
+
+<style global lang="scss">
+  html {
+    height: 100%;
+  }
+
+  body {
+    height: 100%;
+    margin: 0;
+  }
+
+  body > div {
+    height: 100%;
+  }
+
+  .full-height {
+    height: 100%;
+  }
+
+  .sidebar {
+    width: 16vw;
+    display: flex;
+    flex-direction: column;
+    padding: 1em;
+  }
+</style>
