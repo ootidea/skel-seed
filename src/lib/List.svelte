6e2f9ae38136060fe388f0736280ae43b866a073
ootidea <ootidea@gmail.com>
2022-04-19 11:16:55 +0900

    add direction prop to List component

--- src/lib/List.svelte
+++ src/lib/List.svelte
@@ -2,8 +2,9 @@
   import Divider from './Divider.svelte'
   import { type ClassProp, createInjectors, type StyleProp } from './utility'
 
-  type Item = $$Generic<unknown>
+  type Item = $$Generic
   export let items: readonly Item[]
+  export let direction: 'horizontal' | 'vertical' = 'vertical'
 
   let classProp: ClassProp = {}
   export { classProp as class }
@@ -11,11 +12,11 @@
   $: injectors = createInjectors('List', classProp, style)
 </script>
 
-<div {...injectors.attr('root')}>
+<div {...injectors.attr('root')} data-direction={direction}>
   {#each items as item, index}
     {#if index > 0}
       <slot name="divider">
-        <Divider />
+        <Divider direction={direction === 'vertical' ? 'horizontal' : 'vertical'} />
       </slot>
     {/if}
     <slot {item} {index}>
@@ -25,3 +26,15 @@
     <slot name="empty" />
   {/each}
 </div>
+
+<style global lang="scss">
+  .skel-list_root {
+    display: flex;
+    align-items: start;
+    width: max-content;
+
+    &[data-direction='vertical'] {
+      flex-direction: column;
+    }
+  }
+</style>
