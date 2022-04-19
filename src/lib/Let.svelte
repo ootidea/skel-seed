a1d85352c0837fa813f714e690c6d800f5b0d3de
ootidea <ootidea@gmail.com>
2022-04-19 20:58:37 +0900

    add Let component

---
+++ src/lib/Let.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { Arrow } from './utility'
+
+  export let value: unknown = undefined
+
+  function set<T>(newValue: T): T {
+    value = newValue
+    return newValue
+  }
+
+  function capture<Args extends unknown[], R>(f: Arrow<Args, R>): Arrow<Args, R> {
+    return (...args: Args) => {
+      const result = f(...args)
+      value = result
+      return result
+    }
+  }
+
+  function steal<Args extends unknown[], R>(f: Arrow<Args, R>): Arrow<Args, void> {
+    return (...args: Args) => {
+      value = f(...args)
+    }
+  }
+</script>
+
+<slot {value} {set} {capture} {steal} />
