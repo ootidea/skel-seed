7ae527e3c4ed4a5270f865810bdcb9abd86cef81
ootidea <ootidea@gmail.com>
2022-04-22 19:41:12 +0900

    change clickable :active color

--- src/lib/CommonCss.svelte
+++ src/lib/CommonCss.svelte
@@ -6,7 +6,7 @@
     --skel-primary-background-active-color: oklch(95% 0.04 255.4);
     --skel-disabled-text-color: oklch(70% 0 0);
     --skel-clickable-hover-background-color: oklch(97% 0 0);
-    --skel-clickable-active-background-color: oklch(94% 0 0);
+    --skel-clickable-active-background-color: oklch(95% 0 0);
 
     --skel-forward-transition: all 0.2s ease-out;
     --skel-backward-transition: all 0.4s ease-out;
