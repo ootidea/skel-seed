7cb22a89cda350528736c2b27915a5d2c43ede5f
ootidea <ootidea@gmail.com>
2022-04-22 19:38:36 +0900

    add Single select section to ToggleButton page

--- src/routes/ToggleButton.svelte
+++ src/routes/ToggleButton.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
   import MultiSelectToggleButton from '../lib/MultiSelectToggleButton.svelte'
+  import SingleSelectToggleButton from '../lib/SingleSelectToggleButton.svelte'
 </script>
 
 <h1>Toggle button</h1>
 
+<h2>Single select</h2>
+<SingleSelectToggleButton values={['West', 'East']} />
+<SingleSelectToggleButton values={['Enable auto resize']} />
+<SingleSelectToggleButton values={['female', 'male', 'other']} let:value>
+  {{ female: '女性', male: '男性', other: 'その他' }[value]}
+</SingleSelectToggleButton>
+
+<h2>Multi select</h2>
 <MultiSelectToggleButton values={['West', 'East']} />
 <MultiSelectToggleButton values={['Enable auto resize']} />
 <MultiSelectToggleButton values={['female', 'male', 'other']} let:value>
