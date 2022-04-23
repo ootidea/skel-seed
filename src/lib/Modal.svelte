837383a2bb8dd73a54aec3137be3b60b0d6e3238
ootidea <ootidea@gmail.com>
2022-04-24 06:18:41 +0900

    add Modal component

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { type Arrow, type ClassProp, createInjectors, type StyleProp } from './utility'
+
+  export let visible = false
+  export let onClickBackdrop: Arrow<[MouseEvent], unknown> | undefined = undefined
+
+  let classProp: ClassProp = {}
+  export { classProp as class }
+  export let style: StyleProp = {}
+  $: injectors = createInjectors('Modal', classProp, style)
+
+  const open = () => (visible = true)
+  const close = () => (visible = false)
+  const toggle = () => (visible = !visible)
+</script>
+
+{#if visible}
+  <div {...injectors.attr('root')} on:click|self={onClickBackdrop}>
+    <slot {open} {close} {toggle} />
+  </div>
+{/if}
+
+<style global lang="scss">
+  :root {
+    --skel-modal-background-color: oklch(50% 0 0 / 0.05);
+  }
+
+  .skel-modal_root {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 1000;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: var(--skel-modal-background-color);
+  }
+</style>
