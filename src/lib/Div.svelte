2d4ab6e28c50309ae1f839cb4036605f76ff5ff1
ootidea <ootidea@gmail.com>
2022-05-14 07:29:07 +0900

    add Div Props

--- src/lib/Div.svelte
+++ src/lib/Div.svelte
@@ -3,14 +3,22 @@
     AlignItems,
     BackgroundColor,
     Display,
+    Height,
     JustifyContent,
     Position,
     Width,
   } from './styleType'
+  import { BoxSizing } from './styleType'
   import { generateStyleString } from './utility'
 
   export let position: Position | undefined = undefined
+  export let top: string | undefined = undefined
+  export let bottom: string | undefined = undefined
+  export let left: string | undefined = undefined
+  export let right: string | undefined = undefined
   export let display: Display | undefined = undefined
+  export let alignItems: AlignItems | undefined = undefined
+  export let justifyContent: JustifyContent | undefined = undefined
   export let margin: string | undefined = undefined
   export let marginTop: string | undefined = undefined
   export let marginBottom: string | undefined = undefined
@@ -29,15 +37,20 @@
   export let paddingEnd: string | undefined = undefined
   export let paddingBlock: string | undefined = undefined
   export let paddingInline: string | undefined = undefined
+  export let boxSizing: BoxSizing | undefined = undefined
   export let width: Width | undefined = undefined
-  export let alignItems: AlignItems | undefined = undefined
-  export let justifyContent: JustifyContent | undefined = undefined
+  export let height: Height | undefined = undefined
+  export let borderRadius: string | undefined = undefined
   export let backgroundColor: BackgroundColor | undefined = undefined
   let klass: string | undefined = undefined
   export { klass as class }
 
   $: style = generateStyleString({
     position,
+    top,
+    bottom,
+    left,
+    right,
     display,
     margin,
     marginTop,
@@ -57,7 +70,10 @@
     paddingEnd,
     paddingBlock,
     paddingInline,
+    boxSizing,
     width,
+    height,
+    borderRadius,
     alignItems,
     justifyContent,
     backgroundColor,
--- src/lib/styleType.ts
+++ src/lib/styleType.ts
@@ -3,7 +3,6 @@ export type AutoComplete<Literal extends Base, Base = string> =
   | (Base & Record<never, never>)
 
 export type Position = AutoComplete<'static' | 'relative' | 'absolute' | 'fixed' | 'sticky'>
-export type Width = AutoComplete<'max-content' | 'min-content'>
 export type Display = AutoComplete<
   'block' | 'inline' | 'inline-block' | 'flex' | 'inline-flex' | 'grid' | 'inline-grid'
 >
@@ -11,4 +10,7 @@ export type AlignItems = AutoComplete<'start' | 'end' | 'center'>
 export type JustifyContent = AutoComplete<
   'start' | 'end' | 'center' | 'space-between' | 'space-around' | 'space-evenly'
 >
+export type BoxSizing = AutoComplete<'border-box' | 'content-box'>
+export type Width = AutoComplete<'max-content' | 'min-content'>
+export type Height = AutoComplete<'max-content' | 'min-content'>
 export type BackgroundColor = string
