8c638db7cf288c20df76929e3e0811c527708672
ootidea <ootidea@gmail.com>
2022-05-14 06:43:37 +0900

    add Div Props

--- src/lib/Div.svelte
+++ src/lib/Div.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
-  import type { Display, Width } from './styleType'
+  import type { AlignItems, Display, JustifyContent, Position, Width } from './styleType'
+  import { BackgroundColor } from './styleType'
   import { generateStyleString } from './utility'
 
+  export let position: Position | undefined = undefined
   export let width: Width | undefined = undefined
   export let display: Display | undefined = undefined
+  export let alignItems: AlignItems | undefined = undefined
+  export let justifyContent: JustifyContent | undefined = undefined
+  export let backgroundColor: BackgroundColor | undefined = undefined
   let klass: string | undefined = undefined
   export { klass as class }
 
-  $: style = generateStyleString({ width, display })
+  $: style = generateStyleString({
+    position,
+    width,
+    display,
+    alignItems,
+    justifyContent,
+    backgroundColor,
+  })
 </script>
 
 <div class={klass} {style}>
--- src/lib/styleType.ts
+++ src/lib/styleType.ts
@@ -2,7 +2,13 @@ export type AutoComplete<Literal extends Base, Base = string> =
   | Literal
   | (Base & Record<never, never>)
 
+export type Position = AutoComplete<'static' | 'relative' | 'absolute' | 'fixed' | 'sticky'>
 export type Width = AutoComplete<'max-content' | 'min-content'>
 export type Display = AutoComplete<
   'block' | 'inline' | 'inline-block' | 'flex' | 'inline-flex' | 'grid' | 'inline-grid'
 >
+export type AlignItems = AutoComplete<'start' | 'end' | 'center'>
+export type JustifyContent = AutoComplete<
+  'start' | 'end' | 'center' | 'space-between' | 'space-around' | 'space-evenly'
+>
+export type BackgroundColor = string
