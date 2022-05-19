0f6d9bcadcd84a2facdb5503a397f80e8bf7a377
ootidea <ootidea@gmail.com>
2022-05-19 09:48:19 +0900

    improve Popover catalog page

--- src/routes/Popover-component.svelte
+++ src/routes/Popover-component.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import AutoSizeTextArea from '../lib/AutoSizeTextArea.svelte'
+  import Button from '../lib/Button.svelte'
+  import DatePicker from '../lib/DatePicker.svelte'
   import Popover from '../lib/Popover.svelte'
   import PageTitle from './_components/PageTitle.svelte'
   import Sample from './_components/Sample.svelte'
@@ -8,34 +9,27 @@
 <PageTitle>Popover</PageTitle>
 
 <Sample>
-  <Popover on="left" let:toggle>
-    <button on:click={toggle}>hoge</button>
-    <AutoSizeTextArea />
-    <div slot="popover-frame">
-      <AutoSizeTextArea />
-      <button on:click={toggle}>x</button>
+  <Popover let:toggle>
+    <Button onClick={toggle}>Open</Button>
+    <div slot="popover" style="padding: 0.5em 1em">Pop up text</div>
+  </Popover>
+</Sample>
+
+<Sample>
+  <Popover on="right" let:toggle>
+    <Button onClick={toggle}>Open to right</Button>
+    <div slot="popover" style="padding: 1em">
+      <DatePicker onSelect={toggle} />
     </div>
   </Popover>
 </Sample>
 
 <Sample>
-  <Popover on="right" joint="top left" let:toggle>
-    <button on:click={toggle}>hoge</button>
-    <div slot="popover">
-      <AutoSizeTextArea />
-      <button on:click={toggle}>x</button>
+  <Popover on="bottom right" joint="bottom right" let:toggle>
+    <div style="padding: 3rem; background-color: aliceblue">
+      <Button onClick={toggle}>Open in right bottom</Button>
     </div>
-  </Popover>
-</Sample>
-
-<Sample>
-  <Popover on="right" joint="bottom left" let:toggle>
-    <button on:click={toggle}>hoge</button>
-    <div slot="popover">
-      <div>@@@@@@@@@@@</div>
-      <div>@@@@@@@@@@@</div>
-      <div>@@@@@@@@@@@</div>
-    </div>
+    <div slot="popover" style="padding: 0.2em 0.4em">Pop up text</div>
   </Popover>
 </Sample>
 
