4cd62e89b37b811b78dbab61d73c1e569f014b83
ootidea <ootidea@gmail.com>
2022-05-21 19:45:11 +0900

    feat: extract Calendar component from DatePicker component

---
+++ src/lib/Calendar.svelte
@@ -0,0 +1,132 @@
+<script lang="ts">
+  import dayjs from 'dayjs'
+  import CommonCss from './CommonCss.svelte'
+  import IconButton from './IconButton.svelte'
+  import { until } from './utility'
+
+  export let selectedMonth: Date = new Date()
+  let klass = ''
+  export { klass as class }
+
+  // TODO: i18n
+  const dayNames = ['日', '月', '火', '水', '木', '金', '土']
+
+  $: _selectedMonth = dayjs(selectedMonth)
+  $: firstDateOfSelectedMonth = _selectedMonth.date(1)
+  $: firstDateOfSelectedCalendar = firstDateOfSelectedMonth.subtract(firstDateOfSelectedMonth.day(), 'day')
+</script>
+
+<div class="skel-Calendar_root {klass}">
+  <div class="skel-Calendar_year-month-area">
+    <IconButton
+      src="src/assets/chevron-left.svg"
+      onClick={() => (selectedMonth = _selectedMonth.subtract(1, 'month').toDate())}
+      size="1.6em"
+    />
+    <div class="skel-Calendar_year-month">
+      <!-- TODO: i18n -->
+      <span class="skel-Calendar_year">
+        {_selectedMonth.format('YYYY')}年
+      </span>
+      <span class="skel-Calendar_month">
+        {_selectedMonth.format('M')}月
+      </span>
+    </div>
+    <IconButton
+      src="src/assets/chevron-right.svg"
+      onClick={() => (selectedMonth = _selectedMonth.add(1, 'month').toDate())}
+      size="1.6em"
+    />
+  </div>
+
+  <div class="skel-Calendar_grid">
+    <div class="skel-Calendar_day-row">
+      {#each dayNames as dayName, day}
+        <div class="skel-Calendar_cell" data-day={day}>
+          {dayName}
+        </div>
+      {/each}
+    </div>
+
+    {#each until(6) as weakIndex}
+      <div class="skel-Calendar_date-row">
+        {#each dayNames as _, day}
+          {@const date = firstDateOfSelectedCalendar.add(weakIndex, 'week').add(day, 'day')}
+          <div
+            class="skel-Calendar_cell"
+            class:skel-Calendar_next-month={date.isAfter(_selectedMonth, 'month')}
+            class:skel-Calendar_prev-month={date.isBefore(_selectedMonth, 'month')}
+            data-day={day}
+          >
+            <slot name="date-cell" date={date.toDate()}>{date.date()}</slot>
+          </div>
+        {/each}
+      </div>
+    {/each}
+  </div>
+</div>
+
+<CommonCss />
+
+<style global lang="scss">
+  :root {
+    --skel-Calendar_cell-size: 2em;
+    --skel-Calendar_sunday-text-color: oklch(50% 0.6 5);
+    --skel-Calendar_saturday-text-color: oklch(50% 0.6 260);
+  }
+
+  .skel-Calendar_root {
+    width: max-content;
+  }
+
+  .skel-Calendar_year-month-area {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin: 0 1em;
+  }
+
+  .skel-Calendar_year-month {
+    font-weight: bold;
+    color: oklch(50% 0 0);
+  }
+
+  .skel-Calendar_grid {
+    display: grid;
+    grid-template-columns: repeat(7, auto);
+    width: max-content;
+  }
+
+  .skel-Calendar_day-row,
+  .skel-Calendar_date-row {
+    display: contents;
+  }
+
+  .skel-Calendar_cell {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    box-sizing: border-box;
+    width: var(--skel-Calendar_cell-size);
+    height: var(--skel-Calendar_cell-size);
+
+    .skel-Calendar_day-row & {
+      color: oklch(50% 0 0);
+
+      &[data-day='0'] {
+        color: var(--skel-Calendar_sunday-text-color);
+      }
+
+      &[data-day='6'] {
+        color: var(--skel-Calendar_saturday-text-color);
+      }
+    }
+  }
+
+  .skel-Calendar_next-month,
+  .skel-Calendar_prev-month {
+    color: oklch(60% 0 0);
+  }
+</style>
--- src/lib/DatePicker.svelte
+++ src/lib/DatePicker.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import dayjs, { Dayjs } from 'dayjs'
+  import dayjs from 'dayjs'
+  import Calendar from './Calendar.svelte'
   import CommonCss from './CommonCss.svelte'
-  import IconButton from './IconButton.svelte'
-  import { type Arrow, until } from './utility'
+  import type { Arrow } from './utility'
 
   export let selectedDate: Date | undefined = undefined
   export let selectedMonth: Date = new Date()
@@ -10,151 +10,54 @@
   let klass = ''
   export { klass as class }
 
-  // TODO: i18n
-  const dayNames = ['日', '月', '火', '水', '木', '金', '土']
-
-  $: _selectedMonth = dayjs(selectedMonth)
-  $: firstDateOfSelectedMonth = _selectedMonth.date(1)
-  $: firstDateOfSelectedCalendar = firstDateOfSelectedMonth.subtract(firstDateOfSelectedMonth.day(), 'day')
-
-  function onClickDate(date: Dayjs) {
-    selectedDate = date.toDate()
-    onSelect?.(date.toDate())
+  function onClickDate(date: Date) {
+    selectedDate = date
+    onSelect?.(date)
   }
 </script>
 
-<div class="skel-DatePicker_root {klass}">
-  <div class="skel-DatePicker_year-month-area">
-    <IconButton
-      src="src/assets/chevron-left.svg"
-      onClick={() => (selectedMonth = _selectedMonth.subtract(1, 'month').toDate())}
-      size="1.6em"
-    />
-    <div class="skel-DatePicker_year-month">
-      <!-- TODO: i18n -->
-      <span class="skel-DatePicker_year">
-        {_selectedMonth.format('YYYY')}年
-      </span>
-      <span class="skel-DatePicker_month">
-        {_selectedMonth.format('M')}月
-      </span>
-    </div>
-    <IconButton
-      src="src/assets/chevron-right.svg"
-      onClick={() => (selectedMonth = _selectedMonth.add(1, 'month').toDate())}
-      size="1.6em"
-    />
+<Calendar class="skel-DatePicker_root {klass}" bind:selectedMonth>
+  <div
+    slot="date-cell"
+    class="skel-DatePicker_date-cell"
+    class:skel-DatePicker_selected={dayjs(date).isSame(selectedDate, 'date')}
+    let:date
+    on:click={() => onClickDate(date)}
+  >
+    {date.getDate()}
   </div>
-
-  <div class="skel-DatePicker_grid">
-    <div class="skel-DatePicker_day-row">
-      {#each dayNames as dayName, day}
-        <div class="skel-DatePicker_cell" data-day={day}>
-          {dayName}
-        </div>
-      {/each}
-    </div>
-
-    {#each until(6) as weakIndex}
-      <div class="skel-DatePicker_date-row">
-        {#each dayNames as _, day}
-          {@const date = firstDateOfSelectedCalendar.add(weakIndex, 'week').add(day, 'day')}
-          <div
-            class="skel-DatePicker_cell"
-            class:skel-DatePicker_selected={date.isSame(selectedDate, 'date')}
-            class:skel-DatePicker_next-month={date.isAfter(_selectedMonth, 'month')}
-            class:skel-DatePicker_prev-month={date.isBefore(_selectedMonth, 'month')}
-            data-day={day}
-            on:click={() => onClickDate(date)}
-          >
-            {date.date()}
-          </div>
-        {/each}
-      </div>
-    {/each}
-  </div>
-</div>
+</Calendar>
 
 <CommonCss />
 
 <style global lang="scss">
-  :root {
-    --skel-DatePicker_cell-size: 2em;
-    --skel-DatePicker_sunday-text-color: oklch(50% 0.6 5);
-    --skel-DatePicker_saturday-text-color: oklch(50% 0.6 260);
-  }
-
   .skel-DatePicker_root {
     width: max-content;
   }
 
-  .skel-DatePicker_year-month-area {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    margin: 0 1em;
-  }
-
-  .skel-DatePicker_year-month {
-    font-weight: bold;
-    color: oklch(50% 0 0);
-  }
-
-  .skel-DatePicker_grid {
-    display: grid;
-    grid-template-columns: repeat(7, auto);
-    width: max-content;
-  }
-
-  .skel-DatePicker_day-row,
-  .skel-DatePicker_date-row {
-    display: contents;
-  }
-
-  .skel-DatePicker_cell {
+  .skel-DatePicker_date-cell {
     display: flex;
     align-items: center;
     justify-content: center;
 
     box-sizing: border-box;
-    width: var(--skel-DatePicker_cell-size);
-    height: var(--skel-DatePicker_cell-size);
+    width: 100%;
+    height: 100%;
+    border-radius: 99999px;
+    border: transparent 1px solid;
 
-    .skel-DatePicker_day-row & {
-      color: oklch(50% 0 0);
+    cursor: pointer;
 
-      &[data-day='0'] {
-        color: var(--skel-DatePicker_sunday-text-color);
-      }
-
-      &[data-day='6'] {
-        color: var(--skel-DatePicker_saturday-text-color);
-      }
+    &:hover {
+      background-color: var(--skel-clickable-hover-background-color);
     }
 
-    .skel-DatePicker_date-row & {
-      border-radius: 99999px;
-      border: transparent 1px solid;
-
-      cursor: pointer;
-
-      &:hover {
-        background-color: var(--skel-clickable-hover-background-color);
-      }
-
-      &:active {
-        background-color: var(--skel-clickable-active-background-color);
-      }
+    &:active {
+      background-color: var(--skel-clickable-active-background-color);
     }
 
     &.skel-DatePicker_selected {
       border-color: var(--skel-primary-color);
     }
-  }
-
-  .skel-DatePicker_next-month,
-  .skel-DatePicker_prev-month {
-    color: oklch(60% 0 0);
   }
 </style>
