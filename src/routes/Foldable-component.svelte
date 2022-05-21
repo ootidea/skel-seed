0e8c9544efb9cfc5d2aa4d2d521fc68afeba603d
ootidea <ootidea@gmail.com>
2022-05-21 18:27:08 +0900

    feat: add Accordion sample

--- src/routes/Foldable-component.svelte
+++ src/routes/Foldable-component.svelte
@@ -4,6 +4,8 @@
   import PageTitle from './_components/PageTitle.svelte'
   import Sample from './_components/Sample.svelte'
   import SectionTitle from './_components/SectionTitle.svelte'
+
+  let month = 'January'
 </script>
 
 <PageTitle>Foldable</PageTitle>
@@ -37,5 +39,21 @@
       <div slot="title">1.2</div>
       ...
     </Foldable>
+  </Foldable>
+</Sample>
+
+<SectionTitle>Accordion</SectionTitle>
+<Sample>
+  <Foldable unfolded={month === 'January'} onUnfold={() => (month = 'January')}>
+    <div slot="title">January</div>
+    ...
+  </Foldable>
+  <Foldable unfolded={month === 'February'} onUnfold={() => (month = 'February')}>
+    <div slot="title">February</div>
+    ...
+  </Foldable>
+  <Foldable unfolded={month === 'March'} onUnfold={() => (month = 'March')}>
+    <div slot="title">March</div>
+    ...
   </Foldable>
 </Sample>
