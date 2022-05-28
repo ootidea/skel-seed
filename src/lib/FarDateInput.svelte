99837db3bf03515c87055b9514c779f8f4494430
ootidea <ootidea@gmail.com>
2022-05-28 16:08:15 +0900

    feat: set FarDateInput placeholder text color

--- src/lib/FarDateInput.svelte
+++ src/lib/FarDateInput.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import calendar from '/src/assets/calendar.svg'
+  import CommonCss from './CommonCss.svelte'
   import FarDatePicker from './FarDatePicker.svelte'
   import Gravity from './Gravity.svelte'
   import Icon from './Icon.svelte'
@@ -28,7 +29,13 @@
     on:click={() => disabled || toggle()}
     {...$$restProps}
   >
-    <div class="skel-FarDateInput_selected-date">{date?.toLocaleDateString() ?? placeholder}{ZERO_WIDTH_SPACE}</div>
+    <div class="skel-FarDateInput_preview-area">
+      {#if date !== undefined}
+        <div class="skel-FarDateInput_selected-date">{date.toLocaleDateString()}{ZERO_WIDTH_SPACE}</div>
+      {:else}
+        <div class="skel-FarDateInput_placeholder">{placeholder}{ZERO_WIDTH_SPACE}</div>
+      {/if}
+    </div>
     <Gravity>
       <slot name="icon">
         <Icon class="skel-FarDateInput_icon" src={calendar} />
@@ -45,6 +52,8 @@
   />
 </Modal>
 
+<CommonCss />
+
 <style global lang="scss">
   .skel-FarDateInput_root {
     border-radius: var(--skel-input-border-radius);
@@ -56,7 +65,11 @@
     margin-right: 0.3em;
   }
 
-  .skel-FarDateInput_selected-date {
+  .skel-FarDateInput_preview-area {
     padding: 0.4em 0.6em;
+  }
+
+  .skel-FarDateInput_placeholder {
+    color: var(--skel-placeholder-text-color);
   }
 </style>
