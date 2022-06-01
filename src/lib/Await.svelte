04fb834e5c558f04ad7cf337efb4b57889163fbe
ootidea <ootidea@gmail.com>
2022-06-01 09:13:10 +0900

    feat: add Await component

---
+++ src/lib/Await.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  type T = $$Generic
+  export let promise: Promise<T> | undefined
+</script>
+
+{#await promise}
+  <slot result={undefined} {promise} />
+{:then result}
+  <slot {result} {promise} />
+{:catch error}
+  <slot name="catch" {error} {promise} />
+{/await}
